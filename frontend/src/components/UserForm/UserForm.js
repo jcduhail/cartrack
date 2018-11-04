de21b1918caa69f8dfcb24277ea0e2a9c07d5609
jcduhail <jeancharles.duhail@gmail.com>
2018-11-04 16:57:38 +0800

    user form

--- frontend/src/components/UserForm/UserForm.js
+++ frontend/src/components/UserForm/UserForm.js
@@ -12,14 +12,24 @@ class UserForm extends Component {
 	
     constructor(props){
         super(props);
-        console.log(this.props.userInfo);
     }
 
    render() {
    if (sessionStorage.getItem('userData')){
+	   let georef="https://www.google.com/maps/search/?api=1&query="+this.props.userInfo.address.geo.lat+","+this.props.userInfo.address.geo.lng;
+	   let webref="http://"+this.props.userInfo.website;
+	   let mailto="mailto:"+this.props.userInfo.email;
        return (
     		   <div className="user_form">
-    		   {this.props.userInfo.name}({this.props.userInfo.email})
+    		   <b>Name :</b> {this.props.userInfo.name}<br/>
+    		   <b>Email :</b> {this.props.userInfo.email}<br/>
+    		   <b>Address :</b> {this.props.userInfo.address.street}, 
+    		   {this.props.userInfo.address.suite}, 
+    		   {this.props.userInfo.address.zipcode} {this.props.userInfo.address.city}
+    		   <a href={georef} target="_blank"> Open map</a><br/>
+    		   <b>Phone number :</b> {this.props.userInfo.phone} <br/> 
+    		   <b>Website :</b> <a href={webref} target="_blank">{this.props.userInfo.website}</a><br />
+    		   <a href={mailto} target="_top">Contact me</a>
     		   </div>
               );
 
